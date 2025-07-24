9c535ae259dfa2d94740c7889dcf2256a8e2d11f
Allan Medina Ramos <71402787+Medinaallan@users.noreply.github.com>
2025-07-23 22:50:33 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
